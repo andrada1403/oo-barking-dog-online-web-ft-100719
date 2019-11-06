97e886b151ff10dd8ba2dff3d767f9573a3ae1d1
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-11-06 17:34:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
-  def bark
-    puts "woof!"
-  end
   def name=(dog_name)
     @individual_dog_name=dog_name
   end
   def name
     @individual_dog_name
+  end
+  def bark
+    puts "woof!"
   end
 end
